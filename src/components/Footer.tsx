bc10767d1ee2c766bedcf868a44ab9788b5c472a
DeadKidts <102147126+DeadKidts@users.noreply.github.com>
2024-06-17 11:24:23 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ function Footer() {
             <div className='mb-6 lg:mb-0'>
               <Link href='/' className='flex w-fit items-center'>
                 <p className='self-center whitespace-nowrap text-3xl font-medium'>
-                  Elyzen
+                  AniStar
                 </p>
               </Link>
               <p className='font-karla text-[0.7rem] lg:w-[520px] lg:text-[0.8rem]'>
@@ -100,18 +100,18 @@ function Footer() {
           <span className='ms-5 sm:text-center lg:ms-0'>
             © {year}{' '}
             <Link href='/' className='hover:text-white'>
-              Elyzen
+              AniStar
             </Link>{' '}
-            | Made by <span className='font-bold'>codeblitz97</span>
+            | Made by <span className='font-bold'>AniMist</span>
           </span>
           <div className='mt-4 flex lg:mt-0 lg:justify-center'>
             <Link
-              href='https://github.com/codeblitz97/elyzen'
+              href='https://animistd12.vercel.app/'
               target='_blank'
               className=' ms-5 hover:text-gray-900 dark:hover:text-white lg:ms-0'
             >
               <GithubIcon />
-              <span className='sr-only'>GitHub</span>
+              <span className='sr-only'>AniMist</span>
             </Link>
           </div>
         </div>
